6caed32e3411d2c4021020e35a1b9ac57fad35a7
edduardaq <109571281+edduardaq@users.noreply.github.com>
2024-05-31 18:09:35 -0600

    Update model.py US-009

--- models/category/model.py
+++ models/category/model.py
@@ -32,7 +32,7 @@ class CategoryModel():
     __area_group = {
         "$group":{
             "id": "$_id",
-            "area_ids": {
+            "area_id": {
                 "$push":"$area_id"
             },
             "document": {
@@ -46,7 +46,7 @@ class CategoryModel():
             "newRoot": {
                 "$mergeObjects": [
                     "$document",
-                    {"area_ids": "$area_id"}
+                    {"area_id": "$area_id"}
                 ]
             }
         }
@@ -96,23 +96,23 @@ class CategoryModel():
         match_values["$and"] = [{}]
         area_value = dict()
        
-        if dict_match.get("area_ids"):
-            if len(dict_match.get("area_ids")) > 1:
+        if dict_match.get("area_id"):
+            if len(dict_match.get("area_id")) > 1:
                 area_value["$or"] = []
-                for area in dict_match.get("area_ids"):
+                for area in dict_match.get("area_id"):
                     area_tmp = area_value["$or"]
                     area_tmp.append({"area_id._id": ObjectId(area)})
                     area_value["$or"] = area_tmp
                 match_values['$and'].append(area_value)
             else:
                 match_values["area_id._id"] = ObjectId(
-                    dict_match["area_ids"][0])
+                    dict_match["area_id"][0])
 
         if dict_match.get("site_id"):
            match_values["area_id.site_id._id"] = ObjectId(dict_match["site_id"]) 
         
         # if dict_match.get("area_ids"):
-        #     match_values["area_id._id"] = ObjectId(dict_match["area_ids"])
+        #     match_values["area_id._id"] = ObjectId(dict_match["area_id"])
 
         if dict_match.get("country_id"):
             match_values["area_id.site_id.country_id._id"] = ObjectId(dict_match["country_id"])
